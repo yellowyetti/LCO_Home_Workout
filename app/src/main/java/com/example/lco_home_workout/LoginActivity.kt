3810548e7457fec638ff5294a29f55b901eb20d9
yellowyetti <sudoyetti@gmail.com>
2020-04-24 02:01:13 +0930

    Firebase integration

---
+++ app/src/main/java/com/example/lco_home_workout/LoginActivity.kt
@@ -0,0 +1,7 @@
+package com.example.lco_home_workout
+
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity {
+    private lateinit var auth: FirebaseAuth
+}
--- app/src/main/java/com/example/lco_home_workout/MainActivity.kt
+++ app/src/main/java/com/example/lco_home_workout/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.lco_home_workout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+
+
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUer = auth.currentUser
+        Toast.makeText(this, currentUer?.displayName, Toast.LENGTH_LONG).show()
     }
 }
