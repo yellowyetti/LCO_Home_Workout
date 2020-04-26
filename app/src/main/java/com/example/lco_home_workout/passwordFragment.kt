3d566ea53e78f8ddd5b6fe33b87c779ba10ce59b
yellowyetti <sudoyetti@gmail.com>
2020-04-27 02:59:45 +0930

    Finished email authentication.
    Bug: Somehow able to authenticate with empty values. App crashes after 3 runs of doing this.

--- app/src/main/java/com/example/lco_home_workout/passwordFragment.kt
+++ app/src/main/java/com/example/lco_home_workout/passwordFragment.kt
@@ -1,5 +1,6 @@
 package com.example.lco_home_workout
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,12 +8,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.Toast
 import androidx.navigation.fragment.navArgs
+import com.google.firebase.auth.FirebaseAuth
 
 class passwordFragment : Fragment() {
 
     lateinit var v: View
     lateinit var textPassword: EditText
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +29,31 @@ class passwordFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_password, container, false)
+
+        auth = FirebaseAuth.getInstance()
+        val email = requireArguments().getString("email").toString()
+        // val username = requireArguments().getString("username").toString()
         textPassword = v.findViewById(R.id.text_password)
-
-        Log.d("passwordFragment", requireArguments().getString("email").toString())
-
+        textPassword.setOnEditorActionListener { _, actionId, _ ->
+            authenticate(email, textPassword.text.toString())
+            true
+        }
         return v
+    }
+
+    private fun authenticate(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this.requireActivity()) { task ->
+                if(task.isSuccessful) {
+                    Log.d("passwordFragment", "createUserWithEmail:succes")
+                    val intent = Intent(context, MainActivity::class.java)
+                    startActivity(intent)
+                }
+                else {
+                    Log.w("passwordFragment", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(context, task.exception.toString(),
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
